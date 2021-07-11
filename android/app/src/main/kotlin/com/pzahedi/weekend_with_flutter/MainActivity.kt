54da0fc531966315983db4cbb0879932d5a2dded
Payam Zahedi <pzahedi1995@gmail.com>
2021-07-11 20:53:15 +0430

    initialize project

---
+++ android/app/src/main/kotlin/com/pzahedi/weekend_with_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pzahedi.weekend_with_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:weekend_with_flutter/src/spinner.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black87,
+      body: Center(
+        child: Container(
+          width: 400,
+          height: 400,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+          color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
