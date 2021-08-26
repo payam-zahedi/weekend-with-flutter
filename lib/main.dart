6f65ce8c6ee705c09ae5855d38f25c847ff07b99
Payam Zahedi <pzahedi1995@gmail.com>
2021-08-26 09:43:56 +0430

    Home page organized

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:weekend_with_flutter/src/widget/hypno_cycle.dart';
-import 'src/widget/spinner.dart';
+import 'package:weekend_with_flutter/src/widget/spinner.dart';
 
 void main() {
   runApp(MyApp());
 }
+
+final showCases = [
+  TileModel(title: 'Spinner Widget', child: Spinner(color: Colors.blue))
+];
 
 class MyApp extends StatelessWidget {
   @override
@@ -12,6 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        brightness: Brightness.dark,
         primarySwatch: Colors.blue,
       ),
       home: Home(),
@@ -23,12 +27,75 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Container(
-          child: HypnoCycle(),
-        ),
+      appBar: AppBar(
+        title: Text('#WeekendWithFlutter Challenge'),
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(8),
+        itemCount: showCases.length,
+        itemBuilder: (BuildContext context, int index) {
+          final item = showCases[index];
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: ListTile(
+              leading: Text('${index + 1}', style: Theme.of(context).textTheme.headline6,),
+              title: Text(
+                item.title,
+              ),
+              tileColor: Theme
+                  .of(context)
+                  .colorScheme
+                  .surface,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        ScreenContainer(
+                          title: item.title,
+                          child: item.child,
+                        ),
+                  ),
+                );
+              },
+            ),
+          );
+        },
       ),
     );
   }
+}
+
+class ScreenContainer extends StatelessWidget {
+  const ScreenContainer({
+    Key? key,
+    required this.title,
+    required this.child,
+  }) : super(key: key);
+
+  final String title;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(child: child),
+    );
+  }
+}
+
+class TileModel {
+  const TileModel({
+    required this.title,
+    required this.child,
+  });
+
+  final String title;
+  final Widget child;
 }
